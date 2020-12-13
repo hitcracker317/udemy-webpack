7c230d7188a16d42d3b97de413d5a72c61aff2ab
‘hitcracker317;  GIT_AUTHOR_EMAIL=‘dainaso97@gmail.com <a.maeda@branu.jp>
2020-12-13 18:58:34 +0900

    webpackのプラグイン導入(mini-css-extract-plugin, html-webpack-plugin)

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
 const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    // コンパイル後の出力先
     path: path.resolve(__dirname, './dist'),
-    // path.resolve：絶対パスを指定
-    // __dirname：現在のディレクトリ, './dist'：指定する相対パス
-    filename: 'main.js', // コンパイルされたjsのファイル名
+    filename: 'main.js',
   },
   module: {
     rules: [
       {
-        // ファイル名を条件に検知するもの(この場合は.cssというファイルにのみ適応させる)
         test: /\.css/,
         use: [
-          // ruleの条件に合致したらstyle-loaderとcss-loaderを使用する、という意味
-          // 下から優先的に呼ばれるので記述する順番が大事
-          //
           {
-            // style-loader(css-loaderで読み込んだhtmlに抽出させる)の設定
-            loader: 'style-loader'
+            loader: MiniCSSExtractPlugin.loader
           },
           {
-            // css-loader(jsの中にcssのモジュールを読み込ませる)の設定
             loader: 'css-loader'
           }
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    // 使用するプラグインをこちらで定義
+    new MiniCSSExtractPlugin(),
+    new htmlWebpackPlugin({
+      // プラグインのオプション
+      // テンプレート先のHTMLを指定する
+      template: './src/index.html',
+    }),
+  ]
 }
